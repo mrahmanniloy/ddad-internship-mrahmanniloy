3c05412da67fa97f6219da63619cca75555532a9
Maksudur Rahman <niloydbz96@gmail.com>
2021-01-15 14:17:04 +0600

    Second commit - updated Problem_3_solution.py file

--- Problem_3_solution.py
+++ Problem_3_solution.py
@@ -3,15 +3,15 @@
 # Problem Statement:
 """
 Write a program that displays whether a given string is either an Even Palindrome, an
-Odd Palindrome or Not a Palindrome. Solving it using recursion will land you bonus 5
-points
+Odd Palindrome or Not a Palindrome.
+
             Example 1           Example 2               Example 3
 Input       Ababa               sfe3443efs              ssee2344s
 Output   Odd Palindrome     Even Palindrome         Not a Palindrome
 """
 
 # Solution:
-""" The recursive function to check palindrome """
+""" The function to check palindrome """
 def palindrome_checker(string):
     return string == string[::-1]
 
